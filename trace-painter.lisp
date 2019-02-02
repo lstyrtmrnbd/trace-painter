f212057195447c916c0e7194bf8351486c348662
Inaba Tewi <soymilk@airmail.cc>
2019-02-02 15:35:13 -0500

    decompose intersect for debugging

--- trace-painter.lisp
+++ trace-painter.lisp
@@ -25,21 +25,55 @@
   ((position :initarg :pos :accessor pos :type vec3)
    (radius :initarg :r :accessor r :type float)))
 
+(defstruct ray-intersection
+  (distance 0.0 :type float)
+  (point (vec3 0 0 0) :type vec3)
+  (direction (vec3 0 0 0) :type vec3)
+  (normal (vec3 0 0 0) :type vec3))
+
 (defgeneric intersect (shape ray)
   (:documentation "Check if provided ray intersects shape"))
 
+(proclaim '(optimize (debug 3)))
 (defmethod intersect ((shape sphere) ray)
   "Transcription of intersectFast, consider where to use destructive vec ops"
   (with-slots (origin direction) ray
     (with-slots (position radius) shape
       (let* ((rsvec (v- position origin))
-             (rad2 (* radius radius))       ;radius^2
+             (r2 (* radius radius))         ;radius^2
              (interA (v. rsvec direction))) ;cosine angle
         (when (> interA 0.0)
             (let* ((rslen (v. rsvec rsvec)) ;length^2
-                   (interB (- rad2 (+ rslen (* interA interA)))))
-              (when (and (> interB 0.0) (> rad2 interB))
+                   (interB (- r2 (+ rslen (* interA interA)))))
+              (when (and (> interB 0.0) (> r2 interB))
                   (let* ((dist (- interA (sqrt interB)))
-                         (interpt (+ origin (* dist direction)))
+                         (interpt (v+ origin (* dist direction)))
                          (normal (vunit (v- interpt position))))
-                    (make-intersection dist interpt normal direction)))))))))
+                    (make-ray-intersection :distance dist
+                                           :point interpt
+                                           :direction direction
+                                           :normal normal)))))))))
+
+(defun intersect-check-A (origin direction position radius)
+  (let* ((rsvec (v- position origin))
+         (r2 (* radius radius))         ;radius^2
+         (interA (v. rsvec direction)))
+    (when (> interA 0.0)
+      (values rsvec r2 interA))))
+
+(defun intersect-check-B (rsvec r2 interA)
+  (let* ((rslen (v. rsvec rsvec))       ;length^2
+         (interB (- r2 (+ rslen (* interA interA)))))
+    (when (and (> interB 0.0) (> r2 interB))
+      (values interA interB))))
+
+(defun intersect-decompose (sphere ray)
+  (with-slots (origin direction) ray
+    (with-slots (position radius) sphere
+      (multiple-value-call #'intersect-check-B
+        (intersect-check-A origin
+                           direction
+                           position
+                           radius)))))
+
+
